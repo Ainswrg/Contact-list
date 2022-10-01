6562799f93a08243201a5e50a3932f40c0f93d4c
Jaslan <Jaslan.dlnv@gmail.com>
2022-10-01 13:18:00 +0500

    feat: add jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,27 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("OK");
+});
+
+app.post("/auth/login", (req, res) => {
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Vasya Pupkin",
+    },
+    "secret123"
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
 });
 
 app.listen(PORT, () => {
