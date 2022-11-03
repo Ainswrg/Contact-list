9a6b9a84e43faae52738e0a5c72cd081758de418
Jaslan <Jaslan.dlnv@gmail.com>
2022-11-03 16:55:43 +0500

    feat: add cors in back

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 
 import IndexRoute from "./routers/index";
@@ -14,6 +15,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/", IndexRoute);
 
 app.listen(PORT, () => {
